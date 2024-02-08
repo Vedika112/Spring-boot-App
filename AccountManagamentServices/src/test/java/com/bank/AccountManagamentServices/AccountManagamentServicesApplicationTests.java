8df11c516b9c7447b1c11008b42b35574111989b
Vedika112 <88924992+Vedika112@users.noreply.github.com>
2024-02-08 12:32:47 +0530

    Add files via upload

---
+++ AccountManagamentServices/src/test/java/com/bank/AccountManagamentServices/AccountManagamentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.AccountManagamentServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagamentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
